696f3b2b6bca38607a2cf6f609629d5c1340616f
shayan-s <shakeri.shayan@gmail.com>
2020-04-02 15:37:39 -0400

    configured precision for the evaluation output file

--- model.py
+++ model.py
@@ -224,25 +224,147 @@ def runClassifier(n,tweets,smoothingProbability,vocabulary,basque,catalan,galici
         resultClassify,resultProbability = classify(gram, smoothingProbability, vocabulary, basque, catalan, galician, spanish, english,  portuguese)
         arrayResult.append((resultClassify,tweet[1],resultProbability))
 
+    traceFile = open("./outputFiles/trace_" + str(v) + "_" + str(n) + "_" + str(smoothingProbability) + ".txt", "w")
+    counter = 0
     total = len(arrayResult)
     correct = 0
-    traceFile = open("trace_"+str(v)+"_"+str(n)+"_"+str(smoothingProbability)+".txt", "w")
-    counter = 0
+    tpBasque = 0
+    fpBasque = 0
+    fnBasque = 0
+    tpCatalan = 0
+    fpCatalan = 0
+    fnCatalan = 0
+    tpGalician = 0
+    fpGalician = 0
+    fnGalician = 0
+    tpSpanish = 0
+    fpSpanish = 0
+    fnSpanish = 0
+    tpEnglish = 0
+    fpEnglish = 0
+    fnEnglish = 0
+    tpPortuguese = 0
+    fpPortuguese = 0
+    fnPortuguese = 0
+    precisionBasque = 0.0
+    precisionCatalan = 0.0
+    precisionGalician = 0.0
+    precisionSpanish = 0.0
+    precisionEnglish = 0.0
+    precisionPortuguese = 0.0
+    recallBasque = 0.0
+    recallCatalan = 0.0
+    recallGalician = 0.0
+    recallSpanish = 0.0
+    recallEnglish = 0.0
+    recallPortuguese = 0.0
+    f1MeasureBasque = 0.0
+    f1MeasureCatalan = 0.0
+    f1MeasureGalician = 0.0
+    f1MeasureSpanish = 0.0
+    f1MeasureEnglish = 0.0
+    f1MeasurePortuguese = 0.0
     for elem in arrayResult:
         if elem[0] == elem[1]:
+            if elem[0] == 'eu':
+                tpBasque += 1
+            elif elem[0] == 'ca':
+                tpCatalan += 1
+            elif elem[0] == 'gl':
+                tpGalician += 1
+            elif elem[0] == 'es':
+                tpSpanish += 1
+            elif elem[0] == 'en':
+                tpEnglish += 1
+            elif elem[0] == 'pt':
+                tpPortuguese += 1
             correct += 1
-            traceFile.write(allTweetId[counter]+"  "+elem[0]+"  "+str(elem[2])+"  "+elem[1]+"  "+"correct\n")
+            traceFile.write(allTweetId[counter] + "  " + elem[0] + "  " + str(elem[2]) + "  " + elem[1] + "  " + "correct\n")
         else:
-            traceFile.write(allTweetId[counter] + "  " + elem[0] + "  " +str(elem[2])+ "  " + elem[1] + "  " + "wrong\n")
-        counter +=1
+            if elem[0] == 'eu':
+                fpBasque += 1
+            elif elem[0] == 'ca':
+                fpCatalan += 1
+            elif elem[0] == 'gl':
+                fpGalician += 1
+            elif elem[0] == 'es':
+                fpSpanish += 1
+            elif elem[0] == 'en':
+                fpEnglish += 1
+            elif elem[0] == 'pt':
+                fpPortuguese += 1
 
-    accuracy = correct/total*100
-    print(accuracy)
+            if elem[1] == 'eu':
+                fnBasque += 1
+            elif elem[1] == 'ca':
+                fnCatalan += 1
+            elif elem[1] == 'gl':
+                fnGalician += 1
+            elif elem[1] == 'es':
+                fnSpanish += 1
+            elif elem[1] == 'en':
+                fnEnglish += 1
+            elif elem[1] == 'pt':
+                fnPortuguese += 1
+            traceFile.write(allTweetId[counter] + "  " + elem[0] + "  " + str(elem[2]) + "  " + elem[1] + "  " + "wrong\n")
+        counter += 1
+    evaluationFile = open("./outputFiles/eval_" + str(v) + "_" + str(n) + "_" + str(smoothingProbability) + ".txt", "w")
+    accuracy = correct/total
+    if (tpBasque + fpBasque) != 0:
+        precisionBasque = tpBasque / (tpBasque + fpBasque)
+    if (tpCatalan + fpCatalan) != 0:
+        precisionCatalan = tpCatalan / (tpCatalan + fpCatalan)
+    if (tpGalician + fpGalician) != 0:
+        precisionGalician = tpGalician / (tpGalician + fpGalician)
+    if (tpSpanish + fpSpanish) != 0:
+        precisionSpanish = tpSpanish / (tpSpanish + fpSpanish)
+    if (tpEnglish + fpEnglish) != 0:
+        precisionEnglish = tpEnglish/(tpEnglish + fpEnglish)
+    if (tpPortuguese + fpPortuguese) != 0:
+        precisionPortuguese = tpPortuguese / (tpPortuguese + fpPortuguese)
+    if (tpBasque + fnBasque) != 0:
+        recallBasque = tpBasque / (tpBasque + fnBasque)
+    if (tpCatalan + fnCatalan) != 0:
+        recallCatalan = tpCatalan / (tpCatalan + fnCatalan)
+    if (tpGalician + fnGalician) != 0:
+        recallGalician = tpGalician / (tpGalician + fnGalician)
+    if (tpSpanish + fnSpanish) != 0:
+        recallSpanish = tpSpanish / (tpSpanish + fnSpanish)
+    if (tpEnglish + fnEnglish) != 0:
+        recallEnglish = tpEnglish/(tpEnglish + fnEnglish)
+    if (tpPortuguese + fnPortuguese) != 0:
+        recallPortuguese = tpPortuguese / (tpPortuguese + fnPortuguese)
+    if (precisionBasque + recallBasque) != 0:
+        f1MeasureBasque = (2*precisionBasque*recallBasque) / (precisionBasque + recallBasque)
+    if (precisionCatalan + recallCatalan) != 0:
+        f1MeasureCatalan = (2*precisionCatalan*recallCatalan) / (precisionCatalan + recallCatalan)
+    if (precisionGalician + recallGalician) != 0:
+        f1MeasureGalician = (2*precisionGalician*recallGalician) / (precisionGalician + recallGalician)
+    if (precisionSpanish + recallSpanish) != 0:
+        f1MeasureSpanish = (2*precisionSpanish*recallSpanish) / (precisionSpanish + recallSpanish)
+    if (precisionEnglish + recallEnglish) != 0:
+        f1MeasureEnglish = (2*precisionEnglish*recallEnglish) / (precisionEnglish + recallEnglish)
+    if (precisionPortuguese + recallPortuguese) != 0:
+        f1MeasurePortuguese = (2*precisionPortuguese*recallPortuguese) / (precisionPortuguese + recallPortuguese)
+    macroF1 = (f1MeasureBasque + f1MeasureCatalan + f1MeasureGalician + f1MeasureSpanish + f1MeasureEnglish + f1MeasurePortuguese)/6
+    weightedBasque = f1MeasureBasque * (tpBasque + fnBasque)
+    weightedCatalan = f1MeasureCatalan * (tpCatalan + fnCatalan)
+    weightedGalician = f1MeasureGalician * (tpGalician + fnGalician)
+    weightedSpanish = f1MeasureSpanish * (tpSpanish + fnSpanish)
+    weightedEnglish = f1MeasureEnglish * (tpEnglish + fnEnglish)
+    weightedPortuguese = f1MeasurePortuguese * (tpPortuguese + fnPortuguese)
+    weightedAverageF1 = (weightedBasque + weightedCatalan + weightedGalician + weightedSpanish + weightedEnglish + weightedPortuguese)/total
+    evaluationFile.write(str(accuracy)+"\n")
+    evaluationFile.write(str(precisionBasque) + "  " + str(precisionCatalan) + "  " + str(precisionGalician) + "  " + str(precisionSpanish) + "  " + str(precisionEnglish) + "  " + str(precisionPortuguese) + "\n")
+    evaluationFile.write(str(recallBasque) + "  " + str(recallCatalan) + "  " + str(recallGalician) + "  " + str(recallSpanish) + "  " + str(recallEnglish) + "  " + str(recallPortuguese) + "\n")
+    evaluationFile.write(str(f1MeasureBasque) + "  " + str(f1MeasureCatalan) + "  " + str(f1MeasureGalician) + "  " + str(f1MeasureSpanish) + "  " + str(f1MeasureEnglish) + "  " + str(f1MeasurePortuguese) + "\n")
+    evaluationFile.write(str(macroF1) + "  " + str(weightedAverageF1))
     traceFile.close()
+    evaluationFile.close()
 def main():
-    V = 2
-    n = 1
-    smooth = 1
+    V = 1
+    n = 2
+    smooth = 0.5
     vocabularyTraining, languages, trainingTweets = readDocument('data.txt')
     testTweets,tweetID,vocabularyTest = readTestingDocument('test.txt')
     vocabulary = chooseV(V,vocabularyTraining,vocabularyTest)
